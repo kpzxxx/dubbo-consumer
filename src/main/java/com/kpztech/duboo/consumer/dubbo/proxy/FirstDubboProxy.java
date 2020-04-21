5aad78ef70d64cf1e164ca1a01795b400f9e7a20
kangpuzi@youzan.com <kangpuzi@youzan.com>
2020-04-21 18:04:04 +0800

    Async

--- src/main/java/com/kpztech/duboo/consumer/dubbo/proxy/FirstDubboProxy.java
+++ src/main/java/com/kpztech/duboo/consumer/dubbo/proxy/FirstDubboProxy.java
@@ -1,19 +1,50 @@
 package com.kpztech.duboo.consumer.dubbo.proxy;
 
+import com.alibaba.dubbo.rpc.RpcContext;
 import com.kpztech.practice.dubbo.service.FirstDubboService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import lombok.extern.slf4j.Slf4j;
+
 @Component
+@Slf4j
 public class FirstDubboProxy {
 
   @Autowired
   private FirstDubboService firstDubboService;
 
+  private RpcContext context = RpcContext.getContext();
+
   public void test() {
-    System.out.println("I'm consumer, now I have to test something!");
+    log.info("I'm consumer, now I have to test something!");
+
+    // RpcContext可以拿到Dubbo的上下文
+
+    context.setAttachment("Key-KKK", "V-KKK");
+
     firstDubboService.test();
+
+    context.get();
+
+    log.info(context.getRemoteAddressString());
+
+  }
+
+  public String asyncTest() {
+    log.info("Async Test>>> ");
+    Future<String> stringFuture = context.asyncCall(() -> firstDubboService.test());
+    String result = "";
+    try {
+      result = stringFuture.get();
+    } catch (InterruptedException | ExecutionException e) {
+      e.printStackTrace();
+    }
+    return result;
   }
 
 
--- src/main/java/com/kpztech/duboo/consumer/mvc/controller/ConsumerController.java
+++ src/main/java/com/kpztech/duboo/consumer/mvc/controller/ConsumerController.java
@@ -3,6 +3,8 @@ package com.kpztech.duboo.consumer.mvc.controller;
 import com.kpztech.duboo.consumer.dubbo.proxy.FirstDubboProxy;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +14,16 @@ public class ConsumerController {
 
   @Autowired
   private FirstDubboProxy firstDubboProxy;
+
   @RequestMapping("/test")
-  public void test(){
+  public void test() {
     firstDubboProxy.test();
+  }
+
+  @GetMapping("/async")
+  public ResponseEntity<String> async() {
+    String result = firstDubboProxy.asyncTest();
+    return ResponseEntity.ok(result);
   }
 
 }
